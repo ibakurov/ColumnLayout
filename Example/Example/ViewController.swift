55308cc1455a45ad52eb38161e3d0ac2c3498842
Illya <bakurov.illya@gmail.com>
2019-04-02 16:08:37 -0400

    Inited Example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Illya Bakurov on 4/2/19.
+//  Copyright © 2019 Illya Bakurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
